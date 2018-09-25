f29eb6a5541c1ab3367528db1e6aeceb8e4c534d
asvsfs <anasvsfs@gmail.com>
2018-09-25 18:37:38 +0330

    first

---
+++ app/index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// Load APM on production environment
+const config = require('./config');
+const apm = require('./apm');
+
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+const errorHandler = require('./middlewares/errorHandler');
+const logMiddleware = require('./middlewares/log');
+const logger = require('./logger');
+const requestId = require('./middlewares/requestId');
+const responseHandler = require('./middlewares/responseHandler');
+const router = require('./routes');
+
+const koaSwagger = require('koa2-swagger-ui');
+
+const app = new Koa();
+
+app.use(
+  koaSwagger({
+    routePrefix: '/swagger', // host at /swagger instead of default /docs
+    swaggerOptions: {
+      url: '/spec', // example path to json
+    },
+  }),
+);
+
+// Trust proxy
+app.proxy = true;
+
+// Set middlewares
+app.use(
+  bodyParser({
+    enableTypes: ['json', 'form'],
+    formLimit: '10mb',
+    jsonLimit: '10mb'
+  })
+);
+app.use(requestId());
+app.use(
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH'],
+    exposeHeaders: ['X-Request-Id']
+  })
+);
+app.use(responseHandler());
+app.use(errorHandler());
+app.use(logMiddleware({ logger }));
+
+// Bootstrap application router
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+function onError(err, ctx) {
+  if (apm.active)
+    apm.captureError(err);
+  if (ctx == null)
+    logger.error({ err, event: 'error' }, 'Unhandled exception occured');
+}
+
+// Handle uncaught errors
+app.on('error', onError);
+
+// Start server
+if (!module.parent) {
+  const server = app.listen(config.port, config.host, () => {
+    logger.info({ event: 'execute' }, `API server listening on ${config.host}:${config.port}, in ${config.env}`);
+  });
+  server.on('error', onError);
+}
+
+// Expose app
+module.exports = app;
