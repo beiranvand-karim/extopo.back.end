a02162a4d3b6fe462554d466d21fd3e3dfa0f064
asvsfs <anasvsfs@gmail.com>
2018-09-25 23:39:34 +0330

    sample user login

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,6 @@
 
 const Router = require('koa-router');
 const homeController = require('./controllers/home');
-const koaSwagger = require('koa2-swagger-ui');
 const router = new Router();
 router.get('/', homeController.getApiInfo);
 router.get('/spec', homeController.getSwaggerSpec);
