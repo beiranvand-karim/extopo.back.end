7233629b877e148dc9edfa3135dcc25cb60db276
karim <beiranvand.karim@gmail.com>
2018-10-06 12:21:57 +0330

    add remote database connection

--- app/mongoose.js
+++ app/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 module.exports = async () => {
-  await mongoose.connect('mongodb://localhost/extopo', { useNewUrlParser: true });
+  await mongoose.connect('mongodb://karim:karim123@ds235022.mlab.com:35022/extopo', { useNewUrlParser: true });
   mongoose.connection.once('open', () => {
     console.log('connection to databbaes has been made.');
   }).on('error', (error) => {
