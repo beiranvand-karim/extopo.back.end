fa39383a4217246500a33cb883654000ab067977
karim <beiranvand.karim@gmail.com>
2018-10-11 10:37:29 +0330

    read membership API

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@ const signOutController = require('./controllers/signOut');
 const { createExperience, readExperience, readAllExperiences, deleteExperience, updateExperience } = require('./controllers/experience');
 const { createProject, readProject, readAllProjects, updateProject, deleteProject } = require('./controllers/project');
 const { createResume, readResume, readAllResumes, updateResume, deleteResume } = require('./controllers/resume');
-const { createMembership } = require('./controllers/membership');
+const { createMembership, readMembership } = require('./controllers/membership');
 
 const router = new Router();
 
@@ -46,5 +46,6 @@ router.delete('/resume/:id', deleteResume);
 
 
 router.post('/membership', createMembership);
+router.get('/membership/:id', readMembership);
 
 module.exports = router;
