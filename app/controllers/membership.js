e571ef41cff3326cb54da2a5702a311a19d44a6e
karim <beiranvand.karim@gmail.com>
2018-10-12 09:30:55 +0330

    delete membership API

--- app/controllers/membership.js
+++ app/controllers/membership.js
@@ -100,3 +100,25 @@ exports.updateMembership = async ctx => {
     return ctx.body = 'NOT Authenticated';
   }
 };
+
+exports.deleteMembership = async ctx => {
+  if (ctx.isAuthenticated()) {
+    try {
+      // delete section
+      const response = await Membership.deleteOne({ '_id': ctx.params.id });
+      if (response.n === 1) {
+        ctx.status = 200;
+        return ctx.body = 'the membership deleted';
+      }
+      // not found section
+      ctx.status = 404;
+      return ctx.body = 'NOT found';
+    } catch (e) {
+      ctx.status = 500;
+      ctx.body = e.message;
+    }
+  } else {
+    ctx.status = 401;
+    return ctx.body = 'NOT Authenticated';
+  }
+};
