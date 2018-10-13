58262ca0707b9e6dc6089774221634ed6c6a5479
karim <beiranvand.karim@gmail.com>
2018-10-13 13:09:15 +0330

    move resume APIs into separate files

---
+++ app/controllers/resume/readResume.js
@@ -0,0 +1,24 @@
+
+const Resume = require('../../models/resume');
+
+exports.readResume = async ctx => {
+  if (ctx.isAuthenticated()) {
+    try {
+      // found section
+      const response = await Resume.findById(ctx.params.id);
+      if (response) {
+        ctx.status = 200;
+        return ctx.body = response;
+      }
+      // not found section
+      ctx.status = 404;
+      return ctx.body = 'NOT found';
+    } catch (e) {
+      ctx.status = e.code;
+      ctx.body = e;
+    }
+  } else {
+    ctx.status = 401;
+    return ctx.body = 'NOT Authenticated';
+  }
+};
---
+++ app/controllers/resume/updateResume.js
@@ -0,0 +1,35 @@
+
+const Resume = require('../../models/resume');
+
+
+exports.updateResume = async ctx => {
+  if (ctx.isAuthenticated()) {
+    try {
+      // const { name, description, date, employees, employer } = ctx.body;
+      const { skills, experiences, languages, projects } = ctx.request.body;
+
+      const updatedFields = {};
+
+      (skills) && Object.assign(updatedFields, { skills });
+      (experiences) && Object.assign(updatedFields, { experiences });
+      (languages) && Object.assign(updatedFields, { languages });
+      (projects) && Object.assign(updatedFields, { projects });
+
+      // update section
+      const response = await Resume.updateOne({ _id: ctx.params.id }, updatedFields);
+      if (response.n === 1) {
+        ctx.status = 200;
+        return ctx.body = 'the resume updated.';
+      }
+      // not found section
+      ctx.status = 404;
+      return ctx.body = 'NOT found';
+    } catch (e) {
+      ctx.status = 500;
+      ctx.body = e.message;
+    }
+  } else {
+    ctx.status = 401;
+    return ctx.body = 'NOT Authenticated';
+  }
+};
