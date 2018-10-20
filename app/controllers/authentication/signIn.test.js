6291cb419a543b0c50e22c7931ec07780f6291e2
karim <beiranvand.karim@gmail.com>
2018-10-20 13:08:38 +0330

    sign in integration tests

--- app/controllers/authentication/signIn.test.js
+++ app/controllers/authentication/signIn.test.js
@@ -1,55 +1,68 @@
-/* global describe, expect, beforeEach, it */
+/* global describe, it */
 'use strict';
 
-const supertest = require('supertest');
+const request = require('supertest');
 const app = require('../../index');
 const User = require('../../models/user');
 
-describe('signIn', () => {
-  const request = supertest(app.listen());
 
-  beforeEach(async () => {
+const { expect } = require('chai');
 
+function promisedAuthRequest() {
+  const authenticatedagent2b = request.agent(app.listen());
+  return new Promise((resolve, reject) => {
+    authenticatedagent2b
+      .post('/sign-in')
+      .send({
+        'userName': 'beiranvand.karim@gmail.com',
+        'passWord': 'karim'
+      })
+      .end(function (error, response) {
+        if (error) { reject(error); }
+        resolve(authenticatedagent2b);
+      });
+  });
+}
+
+function promisedCookie() {
+  return new Promise((resolve, reject) => {
+    request(app)
+      .post('/sign-in')
+      .send({
+        'userName': 'beiranvand.karim@gmail.com',
+        'passWord': 'karim'
+      })
+      .end(function (error, response) {
+        if (error) { reject(error); }
+        let loginCookie = response.headers['set-cookie'];
+        resolve(loginCookie);
+      });
+  });
+}
+
+
+describe('routes', () => {
+
+  it('hits a private route with superagent authentication', () => {
+    return promisedAuthRequest().then(authenticatedagent => {
+      return authenticatedagent.get('/project').expect(200)
+        .then(res => {
+          expect(res.body.answer).to.equal(42);
+        });
+    });
   });
 
-  describe('POST /sign-in', async () => {
-    it('should encounter an internal server error', async () => {
-      await User.deleteMany({});
-
-      const res = await request
-        .post('/sign-up')
-        .send({
-          'userName': 'beiranvand.karim@gmail.com',
-          'passWord': 'karim'
-        })
-        .set('Content-Type', 'application/json');
-
-      expect(res.status).toBe(500);
-    });
-
-    it('should sign in', async () => {
-      await User.deleteMany({});
-
-      await request
-        .post('/sign-up')
-        .send({
-          'firstName': 'kairm',
-          'lastName': 'beiranvand',
-          'userName': 'beiranvand.karim@gmail.com',
-          'passWord': 'karim',
-          'userType': 'employer'
-        })
-        .set('Content-Type', 'application/json');
-
-      const response = await request
-        .post('/sign-in')
-        .send({
-          'userName': 'beiranvand.karim@gmail.com',
-          'passWord': 'karim'
-        })
-        .set('Content-Type', 'application/json');
-
-      expect(response.status).toBe(200);
+  it('hits a private route with supertest authentication and cookie', () => {
+    return promisedCookie().then(cookie => {
+      console.log('cookie is called', cookie);
+      const req = request(app)
+        .get('/project')
+        .set('cookie', cookie)
+        .expect(200)
+        .then(res => {
+          expect(res.body.answer).to.equal(42);
+        });
+      return req;
     });
   });
 });
