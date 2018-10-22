2a7bfc6b9c26f61cf175f1534e086d51280044a4
karim <beiranvand.karim@gmail.com>
2018-10-22 22:20:13 +0330

    cookie solved

--- app/mongoose.js
+++ app/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 module.exports = async () => {
-  await mongoose.connect('mongodb://karim:karim123@ds235022.mlab.com:35022/extopo', { useNewUrlParser: true });
+  await mongoose.connect('mongodb://karim:karim123@localhost:27017/extopo', { useNewUrlParser: true });
   mongoose.connection.once('open', () => {
     console.log('connection to databbaes has been made.');
   }).on('error', (error) => {
