c0e44ef24372e7c503d581ac313c4d203d19fa74
karim <beiranvand.karim@gmail.com>
2018-10-31 13:18:53 +0330

    add survey schema

---
+++ app/models/survey.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+const surveySchema = new Schema({
+  workForceCount: { type: [Number], required: true },
+  demandedSkills: { type: [String], required: true },
+  projectType: { type: String, required: true },
+  projectDescription: { type: String, required: true }
+});
+
+const Survey = mongoose.model('survey', surveySchema);
+
+module.exports = Survey;
