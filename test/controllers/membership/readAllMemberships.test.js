7d55d18213f95e274ea3e08910fcfa532a372451
karim <beiranvand.karim@gmail.com>
2018-11-08 16:19:18 +0330

    integration tests for read all memberships

--- test/controllers/membership/readAllMemberships.test.js
+++ test/controllers/membership/readAllMemberships.test.js
@@ -1,23 +1,55 @@
-/* global describe, beforeEach, it */
+/* global describe, it, expect, afterEach, beforeAll */
 'use strict';
 
-const supertest = require('supertest');
-const app = require('../../../app/index');
+const request = require('supertest');
+const app = require('../../../app/index').listen();
 const Membership = require('../../../app/models/membership');
+const membership = require('./membership.meta');
+const { signIn } = require('../signInCallback');
 
-describe.skip('Membership ', () => {
-  const request = supertest(app.listen());
+let cookie;
+const route = 'membership';
+const readRoute = `/${route}`;
+const createRoute = `/${route}`;
 
-  beforeEach(async () => {
-    await Membership.deleteMany({});
+beforeAll((done) => {
+  signIn(app)
+    .then(_cookie => {
+      cookie = _cookie;
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+describe(`GET ${readRoute}`, () => {
+  let _id;
+
+  beforeAll(async (done) => {
+    const response = await request(app).post(createRoute)
+      .send(membership)
+      .set('Cookie', cookie);
+    expect(response.status).toEqual(201);
+    _id = response.body._id;
+    done();
   });
 
-  describe('GET /membership', () => {
-    it('should be unauthorized.', async () => {
-      const res = await request
-        .get('/membership')
-        .expect('Content-Type', 'text/plain; charset=utf-8')
-        .expect(401);
-    });
+  it('should return not authenticated 401', async () => {
+    const response = await request(app).get(readRoute);
+    expect(response.status).toEqual(401);
+  });
+
+  it(`should read ${route}(s) 200`, async () => {
+    const response = await request(app).get(readRoute)
+      .set('Cookie', cookie);
+    expect(response.status).toEqual(200);
+  });
+
+  it('should return not found 404', async () => {
+    await Membership.deleteMany({});
+    const response = await request(app).get(readRoute)
+      .set('Cookie', cookie);
+    expect(response.status).toEqual(404);
   });
 });
