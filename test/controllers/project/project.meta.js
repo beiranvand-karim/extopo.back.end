061215f6afced7d9c8832e821d7eff096647fad9
karim <beiranvand.karim@gmail.com>
2018-11-08 15:07:46 +0330

    integration tests for delete project

---
+++ test/controllers/project/project.meta.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  'name': 'my website',
+  'description': 'this is my website',
+  'employer': '5bbc626c1d6c83461f56fad6',
+  'employees': ['5bbc626c1d6c83461f56fad6', '5bbc626c1d6c83461f56fad6']
+};
