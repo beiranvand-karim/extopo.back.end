3021856f17ecec6bea6327191121316c211c5c4a
karim <beiranvand.karim@gmail.com>
2018-11-14 13:53:26 +0330

    integration test for create experience

--- test/controllers/experience/createExperience.test.js
+++ test/controllers/experience/createExperience.test.js
@@ -1,23 +1,45 @@
-/* global describe, beforeEach, it */
+/* global describe, it, expect, afterEach, beforeAll */
 'use strict';
 
-const supertest = require('supertest');
-const app = require('../../../app/index');
-const Experience = require('../../../app/models/experience');
+const request = require('supertest');
+const app = require('../../../app/index').listen();
+const experience = require('./experience.meta');
+const { signIn } = require('../signInCallback');
 
-describe.skip('Experience ', () => {
-  const request = supertest(app.listen());
+let cookie;
+const route = 'experience';
+const createRoute = `/${route}`;
 
-  beforeEach(async () => {
-    await Experience.deleteMany({});
+beforeAll((done) => {
+  signIn(app)
+    .then(_cookie => {
+      cookie = _cookie;
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+describe('POST ' + createRoute, () => {
+  it('should return not authenticated 401', async () => {
+    const response = await request(app).post(createRoute)
+      .send(experience);
+    expect(response.status).toEqual(401);
   });
 
-  describe('GET /experience', () => {
-    it('should be unauthorized.', async () => {
-      const res = await request
-        .get('/experience')
-        .expect('Content-Type', 'text/plain; charset=utf-8')
-        .expect(401);
-    });
+  it(`should create a ${route} 201`, async () => {
+    const response = await request(app).post(createRoute)
+      .send(experience)
+      .set('Cookie', cookie);
+    expect(response.status).toEqual(201);
+  });
+
+  it('should return bad request 400', async () => {
+    const modified = { ...experience, description: 10 };
+    const response = await request(app).post(createRoute)
+      .send(modified)
+      .set('Cookie', cookie);
+    expect(response.status).toEqual(400);
   });
 });
